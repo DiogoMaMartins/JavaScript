fb14b0a2b2340c3b7918071e20a0db6eb84df487
isevenbe <florentin.lefort@gmail.com>
2018-10-03 08:57:10 +0200

    dfg

--- Project RPG/app.js
+++ Project RPG/app.js
@@ -19,16 +19,16 @@ class Arme {
             console.log("Vous avez perdu une charge");
             console.log("Il vous reste " + this.maxUse + charge);
         } else {
-            
+
             console.log("Votre " + this.weaponName + " plus de charge");
-            
+
         }
     }
 
     levelChecker (){
-        
+       // Recuperer le nom de l'arme ici
         if (Pnj.level == this.minLevel){
-        
+
             console.log("Vous utilisez " + this.weaponName);
         } else {
             console.log("vous n'avez pas le niveau pour utilister " + this.weaponName);
@@ -38,14 +38,14 @@ class Arme {
 }
 
 class Pnj {
-    
-    constructor(name, level, life) {
-        
+
+    constructor(name, level, life, weapon) {
+
         this.name = name;
         this.level = level;
         this.life = life;
         this.weapon = Arme;
-        
+
     }
 
     functionRecievedammage(calculDommage) {
@@ -53,24 +53,24 @@ class Pnj {
         console.log("il reste " + this.life + " de vie a " + this.name);
     }
 
-    fonctionAttack(ennemyName, weaponHolder) {
-        
-        armeObjet.levelChecker(Arme.weaponName);
+    fonctionAttack(ennemyName, nameOfweapon) {
+        // this.weapon = Arme;
+        levelChecker(nameOfweapon);
+
         let calculDommage = this.level * blunt.minDammage;
         console.log(this.name + " attaque " + ennemyName.name + " avec " + blunt.weaponName + " et lui inflige " + calculDommage);
         ennemyName.functionRecievedammage(calculDommage);
         // Arme.maxUsechecker();
-        
+
     }
 }
-
-let perso = new Pnj("iSevenBe", 5, 1);
 let blunt = new Arme("blunt", 10, 25, 1);
-let armeObjet = new Arme();
-perso.weapon.name = blunt.name;
+let perso = new Pnj("iSevenBe", 10, 1, blunt);
+console.dir(perso)
+let emptyWeapon = new Arme()
 
 
 let ennemy = new Pnj("Skeleton", 2, 10000);
-ennemy.weapon.name = "Sword";
-ennemy.weapon.dammage = 10;
+// ennemy.weapon.name = "Sword";
+// ennemy.weapon.dammage = 10;
 perso.fonctionAttack(ennemy);
